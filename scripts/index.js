098b0206d8ca3d2a56ec5875422d29485ef29837
AleksandrinaS <opheliace20072000@gmail.com>
2025-03-06 15:23:56 +0500

    1 iteration

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,23 @@
 function createCard(cardData, deleteCard) {
     const template = document.querySelector('#card-template');
     const cardElement = template.content.cloneNode(true);
+    const item = cardElement.querySelector('.card')
     const cardTitle = cardElement.querySelector('.card__title');
     const cardImage = cardElement.querySelector('.card__image');
     const deleteButton = cardElement.querySelector('.card__delete-button');
     cardTitle.textContent = cardData.name;
     cardImage.src = cardData.link;
     cardImage.alt = cardData.name;
-    deleteButton.addEventListener('click', () => {
-        const item = deleteButton.closest('.card');
-        item.remove();
+    deleteButton.addEventListener('click', function() {
+        deleteCard(item);
     });
-    
+
     return cardElement;
 }
 
+function deleteCard(item) {
+    item.remove();
+}
+
 const placesList = document.querySelector('.places__list');
-initialCards.forEach(card => placesList.append(createCard(card)));
+initialCards.forEach(card => placesList.append(createCard(card, deleteCard)));
